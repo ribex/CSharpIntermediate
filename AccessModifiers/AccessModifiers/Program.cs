b8a2c2b642f117133ccc2e56bd25c9dda5355151
ribex <ribexy@gmail.com>
2017-11-28 20:24:01 -0500

    Examples of how to use access modifiers public and private

---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthDate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthDate(new DateTime(1977, 4, 1));
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
