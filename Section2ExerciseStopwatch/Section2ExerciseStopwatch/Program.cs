e832248f240f6f8fcb676c07d73994b2cca5708b
ribex <ribexy@gmail.com>
2017-12-02 13:29:02 -0500

    Add _startable field to ensure stopwatch isn't started twice in a row

--- Section2ExerciseStopwatch/Section2ExerciseStopwatch/Program.cs
+++ Section2ExerciseStopwatch/Section2ExerciseStopwatch/Program.cs
@@ -8,15 +8,34 @@ namespace Section2ExerciseStopwatch
 {
     public class Stopwatch
     {
+
+        private bool _startable;
+
+        public Stopwatch()
+        {
+            _startable = true;
+        }
         public DateTime Start()
         {
+            // check if stopwatch has not yet been started or has been stopped (but not only-started)
+            if (_startable == true)
+            {
+                _startable = false;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+
             return DateTime.Now;
         }
 
         public DateTime Stop()
         {
+            _startable = true;
             return DateTime.Now;
         }
+
 
         public TimeSpan Duration(DateTime start, DateTime stop)
         {
@@ -28,13 +47,21 @@ namespace Section2ExerciseStopwatch
         static void Main(string[] args)
         {
             var stopwatch = new Stopwatch();
-            var start = stopwatch.Start();
+            var start1 = stopwatch.Start();
+            // var unstartable = stopwatch.Start();
             System.Threading.Thread.Sleep(5000);
-            var stop = stopwatch.Stop();
+            var stop1 = stopwatch.Stop();
+            var duration1 = stopwatch.Duration(start1, stop1);
+            var durationSum = new TimeSpan();
+            durationSum += duration1;
+            Console.WriteLine(durationSum);
 
-
-            var duration = stopwatch.Duration(start, stop);
-            Console.WriteLine(duration);
+            var start2 = stopwatch.Start();
+            System.Threading.Thread.Sleep(1000);
+            var stop2 = stopwatch.Stop();
+            var duration2 = stopwatch.Duration(start2, stop2);
+            durationSum += duration2;
+            Console.WriteLine(durationSum);
         }
     }
 }
