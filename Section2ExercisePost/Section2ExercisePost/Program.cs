92b74d2a0db75eb56dae627f98c8b26ec8eece23
ribex <ribexy@gmail.com>
2017-12-02 13:48:31 -0500

    Create skeleton of problem statement

---
+++ Section2ExercisePost/Section2ExercisePost/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section2ExercisePost
+{
+    public class Post
+    {
+        // properties
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        private int VoteCount { get; set; }
+
+        // methods
+        public void UpVote()
+        {
+            
+        }
+
+        public void DownVote()
+        {
+            
+        }
+
+        public void DisplayVote()
+        {
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // create a post
+            var post = new Post();
+
+            // upvote post a few times
+            post.UpVote();
+
+            // downvote post a few times
+            post.DownVote();
+
+            // display current vote value
+            post.DisplayVote();
+        }
+    }
+}
