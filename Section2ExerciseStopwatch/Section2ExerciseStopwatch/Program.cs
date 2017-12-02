a8c9fefe6e6acdb8655bf5eac5c6a53a9c117304
ribex <ribexy@gmail.com>
2017-12-02 12:09:36 -0500

    Basic outline of Stopwatch

---
+++ Section2ExerciseStopwatch/Section2ExerciseStopwatch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section2ExerciseStopwatch
+{
+    public class Stopwatch
+    {
+        public DateTime Start()
+        {
+            return DateTime.Now;
+        }
+
+        public DateTime Stop()
+        {
+            return DateTime.Now;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            var duration = stopwatch.Stop() - stopwatch.Start();
+            Console.WriteLine(duration);
+        }
+    }
+}
