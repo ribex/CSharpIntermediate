e280d2b4a42d6880a1519a76688c2a56ee55a2c0
ribex <ribexy@gmail.com>
2017-12-02 11:46:38 -0500

    Example of indexer

---
+++ Indexers/Indexers/HttpCookie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        private readonly Dictionary<string, string> _dictionary;
+        public DateTime Expiration { get; set; }
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+    }
+}
