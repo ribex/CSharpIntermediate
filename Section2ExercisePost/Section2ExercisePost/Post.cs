2d68f2c81ba39986c4b4ab63df15f3c4f4bac7db
ribex <ribexy@gmail.com>
2017-12-03 13:27:37 -0500

    Move Post to its own class file

---
+++ Section2ExercisePost/Section2ExercisePost/Post.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Section2ExercisePost
+{
+    public class Post
+    {
+        // properties
+        private string _title;
+        private string _description;
+        private DateTime _created;
+        private int _voteCount;
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public int VoteCount { get; set; }
+
+        public Post()
+        {
+            Created = DateTime.Now;
+        }
+
+        public Post(string title, string description)
+            : this()
+        {
+            this.Title = title;
+            this.Description = description;
+            this.VoteCount = 0;
+        }
+
+        // methods
+        public void UpVote()
+        {
+            VoteCount++;
+        }
+
+        public void DownVote()
+        {
+            VoteCount--;
+        }
+
+        public void DisplayVote()
+        {
+            Console.WriteLine("The post entitled " + Title + " has " + VoteCount + " votes.");
+        }
+    }
+}
--- Section2ExercisePost/Section2ExercisePost/Program.cs
+++ Section2ExercisePost/Section2ExercisePost/Program.cs
@@ -1,54 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Section2ExercisePost
 {
-    public class Post
-    {
-        // properties
-        private string _title;
-        private string _description;
-        private DateTime _created;
-        private int _voteCount;
-
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public DateTime Created { get; set; }
-        public int VoteCount { get; set; }
-
-        public Post()
-        {
-            Created = DateTime.Now;
-        }
-
-        public Post(string title, string description)
-            : this()
-        {
-            this.Title = title;
-            this.Description = description;
-            this.VoteCount = 0;
-        }
-
-        // methods
-        public void UpVote()
-        {
-            VoteCount++;
-        }
-
-        public void DownVote()
-        {
-            VoteCount--;
-        }
-
-        public void DisplayVote()
-        {
-            Console.WriteLine("The post entitled " + Title + " has " + VoteCount + " votes.");
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
