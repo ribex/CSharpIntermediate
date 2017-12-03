da32b2cab7ff4646d74a0fc8771a5ae48bd9a0c0
ribex <ribexy@gmail.com>
2017-12-03 13:27:09 -0500

    Finish exercise

--- Section2ExercisePost/Section2ExercisePost/Program.cs
+++ Section2ExercisePost/Section2ExercisePost/Program.cs
@@ -9,25 +9,43 @@ namespace Section2ExercisePost
     public class Post
     {
         // properties
+        private string _title;
+        private string _description;
+        private DateTime _created;
+        private int _voteCount;
+
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime Created { get; set; }
-        private int VoteCount { get; set; }
+        public int VoteCount { get; set; }
+
+        public Post()
+        {
+            Created = DateTime.Now;
+        }
+
+        public Post(string title, string description)
+            : this()
+        {
+            this.Title = title;
+            this.Description = description;
+            this.VoteCount = 0;
+        }
 
         // methods
         public void UpVote()
         {
-            
+            VoteCount++;
         }
 
         public void DownVote()
         {
-            
+            VoteCount--;
         }
 
         public void DisplayVote()
         {
-            
+            Console.WriteLine("The post entitled " + Title + " has " + VoteCount + " votes.");
         }
     }
 
@@ -37,12 +55,18 @@ namespace Section2ExercisePost
         {
 
             // create a post
-            var post = new Post();
+            var post = new Post("Test Post", "This is the description");
 
             // upvote post a few times
             post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
 
             // downvote post a few times
+            post.DownVote();
+            post.DownVote();
             post.DownVote();
 
             // display current vote value
