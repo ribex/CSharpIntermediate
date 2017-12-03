5bba2ddd2913861f7a1b562df6255d7b0ea470c0
ribex <ribexy@gmail.com>
2017-12-03 16:54:37 -0500

    Example of inheritance

---
+++ Inheritance/Inheritance/Text.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Text : PresentationObject
+    {
+        public Text()
+        {
+        }
+
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine("We added a link to "+ url);
+        }
+    }
+}
