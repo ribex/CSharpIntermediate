cd2ad8660893834177e801b2206d313700063c3b
ribex <ribexy@gmail.com>
2017-12-04 20:22:16 -0500

    Various upcasting examples

---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+//            var list = new ArrayList();
+//            list.Add(1);
+//            list.Add("Rebecca");
+//            list.Add(new Text());
+//
+//            var anotherList = new List<int>();
+
+            // StreamReader read = new StreamReader(new MemoryStream());
+
+//            Text text = new Text();
+//            Shape shape = text;
+//
+//            text.Width = 200;
+//            shape.Width = 100;
+//
+//            Console.WriteLine(text.Width); // 100
+        }
+    }
+}
