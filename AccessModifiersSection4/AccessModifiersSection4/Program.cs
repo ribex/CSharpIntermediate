688d7217cc413640466995dcc971c1719a74e752
ribex <ribexy@gmail.com>
2017-12-04 19:44:09 -0500

    Examples of access modifiers with inheritance

---
+++ AccessModifiersSection4/AccessModifiersSection4/Program.cs
@@ -0,0 +1,13 @@
+﻿using Amazon;
+
+namespace AccessModifiersSection4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            Amazon.RateCalculator calculator = new RateCalculator();
+        }
+    }
+}
