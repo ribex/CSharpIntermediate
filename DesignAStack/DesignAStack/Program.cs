28b0ce389e974af8853eff8a4ea65d8ef567459a
ribex <ribexy@gmail.com>
2017-12-05 18:58:25 -0500

    Complete implementation of Stack class

--- DesignAStack/DesignAStack/Program.cs
+++ DesignAStack/DesignAStack/Program.cs
@@ -1,17 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
 namespace DesignAStack
 {
+    public class Stack
+    {
+        private readonly ArrayList _stack = new ArrayList();
+       
+        public void Push(object obj)
+        {
+            if (obj != null)
+            {
+                _stack.Add(obj);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public object Pop()
+        {
+            if (_stack.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var popped = _stack[_stack.Count - 1];
+            _stack.Remove(popped);
+            return popped;
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             var stack = new Stack();
             stack.Push(1);
+            // stack.Clear();
             stack.Push(2);
             stack.Push(3);
 
@@ -19,10 +52,22 @@ namespace DesignAStack
             Console.WriteLine(stack.Pop());
             Console.WriteLine(stack.Pop());
 
+            stack.Push(4);
+            stack.Push(5);
+
+            Console.WriteLine(stack.Pop());
+
+            stack.Clear();
+
+            stack.Push(6);
+            Console.WriteLine(stack.Pop());
+
             // output:
             // 3
             // 2
             // 1
+            // 5
+            // 6
         }
     }
 }
