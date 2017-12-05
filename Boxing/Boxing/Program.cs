653990adaa7eb0679ca7c1e6dbd47c78eed06eca
ribex <ribexy@gmail.com>
2017-12-04 20:48:28 -0500

    Examples of boxing and unboxing

---
+++ Boxing/Boxing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // no type safety & boxing
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Rebecca");
+            list.Add(DateTime.Today);
+
+            var anotherList = new List<int>();
+            // every added element must be an int
+            // anotherList.Add(); 
+            var names = new List<string>();
+            // every added element must be a string
+            // names.Add();
+
+        }
+    }
+}
