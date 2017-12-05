8108a7c474438d40a68af70fefbd7e419d1133fe
ribex <ribexy@gmail.com>
2017-12-04 20:06:42 -0500

    Example of constructors with inheritance

---
+++ ConstructorsAndInheritance/ConstructorsAndInheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsAndInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("ABCXYZ");
+        }
+    }
+}
