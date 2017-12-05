4073f9a8e394aa0423d9cddf688b9f06b9d05c54
ribex <ribexy@gmail.com>
2017-12-05 17:29:34 -0500

    Initial commit of Design A Stack exercise with outline of what it should do: push and pop objects onto and off of a stack.

---
+++ DesignAStack/DesignAStack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignAStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            // output:
+            // 3
+            // 2
+            // 1
+        }
+    }
+}
