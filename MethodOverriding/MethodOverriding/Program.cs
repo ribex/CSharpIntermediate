46ddc9c97f5e405254ac687fc14649e6613d2184
ribex <ribexy@gmail.com>
2017-12-05 19:20:12 -0500

    Method overriding: initial example that needs improvement

--- MethodOverriding/MethodOverriding/Program.cs
+++ MethodOverriding/MethodOverriding/Program.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MethodOverriding
 {
-    public class Shape
-    {
-        public int Width { get; set; }
-        public int Height { get; set; }
-        public Position Position { get; set; }
-        public ShapeType Type { get; set; }
-    }
-
-
     class Program
     {
         static void Main(string[] args)
