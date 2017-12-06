88c11b200a9c57c059c13986a05bef5466d7e817
ribex <ribexy@gmail.com>
2017-12-05 19:11:48 -0500

    Move Stack class to its own class file

--- DesignAStack/DesignAStack/Program.cs
+++ DesignAStack/DesignAStack/Program.cs
@@ -1,43 +1,7 @@
 ﻿using System;
-using System.Collections;
 
 namespace DesignAStack
 {
-    public class Stack
-    {
-        private readonly ArrayList _stack = new ArrayList();
-       
-        public void Push(object obj)
-        {
-            if (obj != null)
-            {
-                _stack.Add(obj);
-            }
-            else
-            {
-                throw new InvalidOperationException();
-            }
-        }
-
-        public object Pop()
-        {
-            if (_stack.Count == 0)
-            {
-                throw new InvalidOperationException();
-            }
-
-            var popped = _stack[_stack.Count - 1];
-            _stack.Remove(popped);
-            return popped;
-        }
-
-        public void Clear()
-        {
-            _stack.Clear();
-        }
-
-    }
-
     class Program
     {
         static void Main(string[] args)
