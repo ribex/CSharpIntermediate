c484b69a490760b66acadd4fc0a9c13fd7793c84
ribex <ribexy@gmail.com>
2017-12-07 20:09:25 -0500

    Example of multiple inheritance with interfaces

---
+++ MultipleInheritance/MultipleInheritance/UiControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultipleInheritance
+{
+    public class UiControl
+    {
+        public string Id { get; set; }
+        public Size Size { get; set; }
+        public Position TopLeft { get; set; }
+
+        public virtual void Draw()
+        {
+            
+        }
+
+        public void Focus()
+        {
+            Console.WriteLine("Received focus.");
+        }
+    }
+}
