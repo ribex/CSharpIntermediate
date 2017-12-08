d1ba7904dcc889e0a62afd83575cf3c1d96782fb
ribex <ribexy@gmail.com>
2017-12-07 20:29:07 -0500

    Example of polymorphism with interfaces

---
+++ Polymorphism/Polymorphism/VideoEncoder.cs
@@ -0,0 +1,19 @@
+﻿namespace Polymorphism
+{
+    public class VideoEncoder
+    {
+        private readonly MailService _mailService;
+
+        public VideoEncoder()
+        {
+            _mailService = new MailService();
+        }
+
+        public void Encode(Video video)
+        {
+            // video encoding logic
+
+            _mailService.Send(new Mail());
+        }
+    }
+}
