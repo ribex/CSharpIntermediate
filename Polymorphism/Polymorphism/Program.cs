1fe7e46775709143d26f3da3eb3367a1bd245d8c
ribex <ribexy@gmail.com>
2017-12-07 20:36:47 -0500

    Change from single mailservice to allow for mail and sms

--- Polymorphism/Polymorphism/Program.cs
+++ Polymorphism/Polymorphism/Program.cs
@@ -10,6 +10,8 @@ namespace Polymorphism
         static void Main(string[] args)
         {
             var encoder = new VideoEncoder();
+            encoder.RegisterNotificationChannel(new MailNotificationChannel());
+            encoder.RegisterNotificationChannel(new SmsNotificationChannel());
             encoder.Encode(new Video());
         }
     }
--- Polymorphism/Polymorphism/VideoEncoder.cs
+++ Polymorphism/Polymorphism/VideoEncoder.cs
@@ -1,19 +1,27 @@
-﻿namespace Polymorphism
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Polymorphism
 {
     public class VideoEncoder
     {
-        private readonly MailService _mailService;
-
+        private readonly IList<INotificationChannel> _notificationChannels;
         public VideoEncoder()
         {
-            _mailService = new MailService();
+            _notificationChannels = new List<INotificationChannel>();
         }
 
         public void Encode(Video video)
         {
             // video encoding logic
 
-            _mailService.Send(new Mail());
+            foreach (var channel in _notificationChannels)
+                channel.Send(new Message());
+        }
+
+        public void RegisterNotificationChannel(INotificationChannel channel)
+        {
+            _notificationChannels.Add(channel);
         }
     }
 }
